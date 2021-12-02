62dba13bc68d1f49788124d2742f384b9f64d899
Emmanuel Torres <emmanuel.torresamaya@gmail.com>
2021-12-02 10:24:11 -0700

    Added s3 client and put command to the bucket service

--- server/services/s3-bucket-service.js
+++ server/services/s3-bucket-service.js
@@ -1,14 +1,21 @@
-const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3')
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
+const path  = require('path')
+const fs = require('fs')
+
+const file = 'C:\\Users\\emman\\OneDrive\\Desktop\\Screenshot 2021-12-02 082912.png'
+const fileKey = "test-images/"+path.basename(file)
+const fileStream = fs.createReadStream(file)
 
 const client = new S3Client({
-    region: "us-west-2",
-    credentials: ''
+    region: "us-west-2"
 })
-const command = new GetObjectCommand({
-    Bucket: 'arn:aws:s3:us-west-2:332716811927:accesspoint/portfolio-api-access-point',
-    ExpectedBucketOwner: 'd520796b3e52b44b6d02c0a5af598d88d89ad71aac417fc58c9c99aa1cc3ff07',
-    IfMatch: 'fd5752b807699e8dc94aa6961f8036b4',
-    Key: 'test-images/87101707_573975746525423_5740685671887011840_o.png'
+
+const putCommand = new PutObjectCommand({
+    Bucket: 'emmanuel-portfolio-image-bucket',
+    Key: fileKey,
+    Body: fileStream,
+    ContentType: 'image/png',
+    ACL: 'public-read'
 });
-const response = client.send(command);
-console.log(response.Body);
+
+const response = client.send(putCommand).then(r => console.log(r))
