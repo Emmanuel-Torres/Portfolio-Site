adf8300dda1d4c31e231ad47e88c127d46b76b82
Emmanuel Torres <emmanuel.torresamaya@gmail.com>
2021-12-11 23:20:36 -0700

    breaking the test case again

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const TestComponent = () => <p>Sample Content</p>
+  const TestComponent = () => <p>Sample</p>
   render(<TestComponent />);
   const linkElement = screen.getByText(/Sample Content/);
   expect(linkElement).toBeInTheDocument();
