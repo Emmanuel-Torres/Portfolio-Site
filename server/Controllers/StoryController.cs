097b71f5aef0f90b72ec759d5f0e18cecf9b816f
Emmanuel Torres <emmanuel.torresamaya@gmail.com>
2022-01-20 14:33:56 -0700

    Added endpoints to get posts

--- server/Controllers/StoryController.cs
+++ server/Controllers/StoryController.cs
@@ -8,13 +8,26 @@ namespace server.Controllers;
 public class StroyController : ControllerBase
 {
     private readonly ILogger<StroyController> _logger;
+    private IEnumerable<Story> Stories = new List<Story> { new Story("Hello", "Hello", DateTime.Now) };
 
     public StroyController(ILogger<StroyController> logger)
     {
         _logger = logger;
     }
 
-    
+    [HttpGet]
+    [Route("posts")]
+    public Task<IEnumerable<Story>> GetStories()
+    {
+        return Task.FromResult<IEnumerable<Story>>(Stories);
+    }
+
+    [HttpGet]
+    [Route("posts/{id:int}")]
+    public Task<Story> GetStoryById(int id)
+    {
+        return Task.FromResult<Story>(new Story("Hello", "NewTitle", DateTime.Now, id: id));
+    }
 
     // [HttpGet]
     // [Route("Hello")]
--- server/Models/Story.cs
+++ server/Models/Story.cs
@@ -2,17 +2,20 @@ namespace server.Models;
 
 public class Story
 {
-    public int? ID { get; set; }
+    public int? Id { get; set; }
     public string Title { get; set; }
     public string? MainImageUrl { get; set; }
     public string? Content { get; set; }
     public string Categoty { get; set; }
     public DateTime PostedOn { get; set; }
 
-    public Story(string title, string category, string postedOn)
+    public Story(string title, string category, DateTime postedOn, int id = -1, string? mainImageUrl = null, string? content = null)
     {
         this.Title = title;
         this.Categoty = category;
         this.PostedOn = postedOn;
+        this.Id = id;
+        this.MainImageUrl = mainImageUrl;
+        this.Content = content;
     }
 }
