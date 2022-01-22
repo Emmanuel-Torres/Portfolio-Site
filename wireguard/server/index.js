2588c567cadc4b659755e6b9c6c8a2b255c110c3
Emmanuel Torres <emmanuel.torresamaya@gmail.com>
2022-01-21 21:47:57 -0700

    added html page to host form

--- wireguard/server/index.js
+++ wireguard/server/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static('pages/AddClient.html'))
 
 app.get('/', async (req, res) => {
-    res.send('Hello World');
+    res.sendFile(path.join(__dirname+'/pages/AddClient.html'))
 })
 
-app.listen()
+app.post('/addconfig', async (req, res) => {
+    
+})
+
+app.listen(80, () => {
+    console.log(`Running at wireguard:3000`)
+})
--- wireguard/server/services/db-service.js
+++ wireguard/server/services/db-service.js
@@ -1,12 +1,23 @@
 const { Pool } = require("pg")
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
+    user: "postgres",
+    password: "password",
+    host: "wireguard_db",
+    database: "postgres",
 })
 
-const addConfig = async () => {
+const addConfig = async (config) => {
+    const res = await pool.query(`
+        INSERT INTO wireguard.client (
+            client_name,
+            client_ip_address,
+            client_allowed_ip_range,
+            client_public_key,
+            client_private_key)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING *`,
+        [config.name, config.ipAddress, config.allowedIpRange, config.publicKey, config.privateKey]);
     
+    return res.rows[0];
 }
