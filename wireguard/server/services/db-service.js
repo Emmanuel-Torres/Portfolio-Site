86857a3f7a2b5b7c51c1e26b6e61fbad2599952e
Emmanuel Torres <emmanuel.torresamaya@gmail.com>
2022-01-22 11:31:07 -0700

    added env to wg api and added server block to swag config

--- wireguard/server/services/db-service.js
+++ wireguard/server/services/db-service.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg")
 
 const pool = new Pool({
-    user: "postgres",
-    password: "password",
-    host: "localhost",
-    database: "postgres",
-    port: 5432,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    port: process.env.POSTGRES_PORT,
 })
 
 const addConfig = async (config) => {
