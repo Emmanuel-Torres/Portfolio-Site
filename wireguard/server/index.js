e58474afdced96adcb498c9bc798c411c825e6b8
Emmanuel Torres <emmanuel.torresamaya@gmail.com>
2022-01-21 22:02:37 -0700

    We are able to add from our form to the databse

--- wireguard/server/index.js
+++ wireguard/server/index.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { dbService } = require("./services/db-service")
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('pages/AddClient.html'))
 
 app.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname+'/pages/AddClient.html'))
+    res.sendFile(path.join(__dirname + '/pages/AddClient.html'))
 })
 
 app.post('/addconfig', async (req, res) => {
-    
+    const config = {
+        name: req.body.name,
+        ipAddress: req.body.ipAddress,
+        allowedIpRange: req.body.allowedIpRange,
+        publicKey: req.body.publicKey,
+        privateKey: req.body.privateKey,
+        dateAdded: new Date()
+    }
+
+    try {
+        res.json(await dbService.addConfig(config))
+    }
+    catch (ex) {
+        console.error(ex);
+    }
 })
 
 app.listen(80, () => {
--- wireguard/server/services/db-service.js
+++ wireguard/server/services/db-service.js
@@ -3,8 +3,9 @@ const { Pool } = require("pg")
 const pool = new Pool({
     user: "postgres",
     password: "password",
-    host: "wireguard_db",
+    host: "localhost",
     database: "postgres",
+    port: 5432,
 })
 
 const addConfig = async (config) => {
@@ -14,10 +15,15 @@ const addConfig = async (config) => {
             client_ip_address,
             client_allowed_ip_range,
             client_public_key,
-            client_private_key)
-        VALUES ($1, $2, $3, $4, $5)
+            client_private_key,
+            client_date_added)
+        VALUES ($1, $2, $3, $4, $5, $6)
         RETURNING *`,
-        [config.name, config.ipAddress, config.allowedIpRange, config.publicKey, config.privateKey]);
+        [config.name, config.ipAddress, config.allowedIpRange, config.publicKey, config.privateKey, config.dateAdded]);
     
     return res.rows[0];
+}
+
+module.exports.dbService = {
+    addConfig
 }
