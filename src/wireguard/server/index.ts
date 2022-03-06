bc90748fa96b876f6e9fe720b965b7aafd457610
Emmanuel Torres <emmanuel.torresamaya@gmail.com>
2022-03-05 18:34:43 -0700

    removed dependency replaced tslint with eslint

--- src/wireguard/server/index.ts
+++ src/wireguard/server/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import peerService from "./services/peer-service";
 import userService from "./services/user-service";
 const app = express();
@@ -6,77 +6,71 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello');
-})
-
-app.get('/api/wgservice/status', (req, res) => {
-    try {
-        res.send(peerService.getStatus());
-    }
-    catch (ex) {
-        // console.error(ex);
-        res.sendStatus(500);
-    }
-})
-
-app.get('/api/wgservice/restart', (req, res) => {
-    try {
-        peerService.restartService();
-        res.sendStatus(200);
-    }
-    catch (ex) {
-        // console.error(ex);
-        res.sendStatus(500);
-    }
-})
-
-app.get('/api/wgservice/peers', (req, res) => {
-    try {
-        res.send(peerService.getPeers());
-    }
-    catch (ex) {
-        // console.error(ex);
-        res.sendStatus(500);
-    }
-})
-
-app.post('/api/wgservice/addconfig', async (req, res) => {
-    try {
-        const configPath = await peerService.addConfig(req.body);
-        res.download(configPath);
-    }
-    catch (ex) {
-        // console.error(ex);
-        res.sendStatus(500);
-    }
-})
-
-app.post('/api/wgservice/removeconfig', async (req, res) => {
-    try {
-        // console.log(req.body)
-        peerService.removeConfig(req.body.publicKey)
-    }
-    catch (ex) {
-        // console.error(ex);
-        res.sendStatus(500);
-    }
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello");
 });
 
-app.post('/api/user/adduser', async (req, res) => {
-    try {
-        await userService.addUser(req.body.user);
-        res.sendStatus(200);
+app.get("/api/wgservice/status", (req: Request, res: Response) => {
+  try {
+    res.send(peerService.getStatus());
+  } catch (ex) {
+    // console.error(ex);
+    res.sendStatus(500);
+  }
+});
+
+app.get("/api/wgservice/restart", (req: Request, res: Response) => {
+  try {
+    peerService.restartService();
+    res.sendStatus(200);
+  } catch (ex) {
+    // console.error(ex);
+    res.sendStatus(500);
+  }
+});
+
+app.get("/api/wgservice/peers", (req: Request, res: Response) => {
+  try {
+    res.send(peerService.getPeers());
+  } catch (ex) {
+    // console.error(ex);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/api/wgservice/addconfig", async (req: Request, res: Response) => {
+  try {
+    const configPath = await peerService.addConfig(req.body);
+    res.download(configPath);
+  } catch (ex) {
+    // console.error(ex);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/api/wgservice/removeconfig", async (req: Request, res: Response) => {
+  try {
+    // console.log(req.body)
+    peerService.removeConfig(req.body.publicKey);
+  } catch (ex) {
+    // console.error(ex);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/api/user/adduser", async (req: Request, res: Response) => {
+  try {
+    await userService.addUser(req.body.user);
+    res.sendStatus(200);
+  } catch (ex) {
+    // console.error(ex);
+    if (ex === 400) {
+      res.sendStatus(400);
     }
-    catch(ex) {
-        // console.error(ex);
-        if (ex === 400){
-            res.sendStatus(400);
-        }
-        res.sendStatus(500);
-    }
+    res.sendStatus(500);
+  }
 });
 
 app.listen(process.env.API_PORT || 3000, () => {
-    // console.log(`Running at wireguard:3000`)
-})
+  // console.log(`Running at wireguard:3000`)
+});
