0ae1b91db52d545d4bd2636078656b797fa2f2c2
Emmanuel Torres <emmanuel.torresamaya@gmail.com>
2022-03-11 03:48:38 -0700

    navbar mobile responsive

--- src/portfolio/client/src/components/UI/NavBar/NavBar.tsx
+++ src/portfolio/client/src/components/UI/NavBar/NavBar.tsx
@@ -1,4 +1,6 @@
 import { FC } from "react";
+import { FaHome } from "react-icons/fa";
+import { VscBook, VscFilePdf, VscInfo } from "react-icons/vsc"
 import { NavLink } from "react-router-dom";
 import styles from "./NavBar.module.css";
 
@@ -6,23 +8,40 @@ const NavBar: FC = (): JSX.Element => {
     return (
         <nav className={styles.navbar}>
             <ul className={styles['navbar-nav']}>
-                <li className={styles['navbar-item']}>
-                    <NavLink className={styles.link} to='/'>Emmanuel Torres</NavLink>
+                <li className={`${styles['navbar-item']} ${styles['navbar-logo']}`}>
+                    <NavLink className={styles['navbar-link']} to='/'>
+                        <span className={styles['link-text']}>Emmanuel Torres</span>
+                    </NavLink>
                 </li>
                 <li className={styles['navbar-item']}>
-                    <NavLink className={styles.link} to='/'>Home</NavLink>
+                    <NavLink className={styles['navbar-link']} to='/'>
+                        <span className={styles['link-text']}>Home</span>
+                        <FaHome className={styles['link-svg']} />
+                    </NavLink>
                 </li>
                 <li className={styles['navbar-item']}>
-                    <NavLink className={styles.link} to='/stories'>Stories</NavLink>
+                    <NavLink className={styles['navbar-link']} to='/stories'>
+                        <span className={styles['link-text']}>Stories</span>
+                        <VscBook className={styles['link-svg']} />
+                    </NavLink>
+                </li>
+                {/* <li className={styles['navbar-item']}>
+                    <NavLink className={styles['navbar-link']} to='/projects'>Projects</NavLink>
+                </li> */}
+                <li className={styles['navbar-item']}>
+                    <NavLink className={styles['navbar-link']} to='/about'>
+                        <span className={styles['link-text']}>About Me</span>
+                        <VscInfo className={styles['link-svg']} />
+                    </NavLink>
                 </li>
                 <li className={styles['navbar-item']}>
-                    <NavLink className={styles.link} to='/projects'>Projects</NavLink>
-                </li>
-                <li className={styles['navbar-item']}>
-                    <NavLink className={styles.link} to='/about'>About Me</NavLink>
+                    <NavLink className={styles['navbar-link']} to='/'>
+                        <span className={styles['link-text']}>Resume</span>
+                        <VscFilePdf className={styles['link-svg']} />
+                    </NavLink>
                 </li>
             </ul>
-            
+
         </nav>
     )
 };
