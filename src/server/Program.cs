49a8efdd64c55853d39b3f2ca231227c3e9ea9ca
Emmanuel-Torres <emmanuel.torresamaya@gmail.com>
2023-02-17 00:35:15 -0700

    I am able to serve react app from dotnet api

--- src/server/Program.cs
+++ src/server/Program.cs
@@ -15,24 +15,19 @@ builder.Services.AddTransient<IStoryRepo, StoryRepo>();
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
     app.UseDeveloperExceptionPage();
     app.UseMigrationsEndPoint();
 }
 else
 {
-    // app.UseHttpsRedirection();
+    app.UseHsts();
 }
 
 using (var scope = app.Services.CreateScope())
@@ -43,5 +38,14 @@ using (var scope = app.Services.CreateScope())
     // context.Database.Migrate();
 }
 
-app.MapControllers();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern:"{controller}/{action=Index}/{id?}"
+);
+
+app.MapFallbackToFile("index.html");
 app.Run();
