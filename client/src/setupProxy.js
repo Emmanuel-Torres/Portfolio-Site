ab3c5aa457af0fb95d24e490581df1f761250892
Emmanuel-Torres <emmanuel.torresamaya@gmail.com>
2024-02-28 23:51:19 -0700

    fixed default.conf and proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,14 +2,6 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app) {
     app.use(
-        '/api',
-        createProxyMiddleware({
-            target: 'http://localhost:5000',
-            changeOrigin: true,
-        })
-    );
-
-    app.use(
         '/portfolio',
         createProxyMiddleware({
             target: 'https://emmanuel-portfolio-image-bucket.s3.us-west-2.amazonaws.com',
