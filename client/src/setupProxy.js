b93f10101eff743ff851bcefae69b032f7672429
Emmanuel-Torres <emmanuel.torresamaya@gmail.com>
2024-03-10 22:28:19 -0600

    removed setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,11 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function(app) {
-    app.use(
-        '/portfolio',
-        createProxyMiddleware({
-            target: 'https://emmanuel-portfolio-image-bucket.s3.us-west-2.amazonaws.com',
-            changeOrigin: true,
-        })
-    )
-};
+// module.exports = function(app) {
+//     app.use(
+//         '/portfolio',
+//         createProxyMiddleware({
+//             target: 'https://emmanuel-portfolio-image-bucket.s3.us-west-2.amazonaws.com',
+//             changeOrigin: true,
+//         })
+//     )
+// };
