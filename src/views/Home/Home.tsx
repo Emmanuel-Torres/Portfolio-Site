05039f101b2771aab37f0a4ace3afc32155f5451
Emmanuel-Torres <emmanuel.torresamaya@gmail.com>
2025-01-17 02:35:25 -0600

    changed animation speeds for content and image in home page

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -5,17 +5,19 @@ import coverPhoto from '../../assets/images/cover_photo.jpg'
 
 const Home: FC = (): JSX.Element => {
     return (
-        <main>
+        <main className={styles.main}>
             <img src={coverPhoto} className={styles.image} alt="main" />
             <div className={styles.container}>
                 <h1 className={styles.header}>Hello! My name is Emmanuel</h1>
-                <p className={styles.content}>
-                    I am a Software Engineer that specializes in Web Development, who is based in Utah.
-                    I love to use React for developing Front End applications and ASP.NET for developing the Back End.
-                    I am always looking for new things to learn and new problems to solve.
-                    My goal is to keep growing as a Software Engineer; writing code to solve problems.
-                </p>
-                <SocialMedia />
+                <div className={styles.content}>
+                    <p>
+                        I am a Software Engineer that specializes in Web Development, who is based in Utah.
+                        I love to use React for developing Front End applications and ASP.NET for developing the Back End.
+                        I am always looking for new things to learn and new problems to solve.
+                        My goal is to keep growing as a Software Engineer; writing code to solve problems.
+                    </p>
+                    <SocialMedia />
+                </div>
             </div>
         </main>
     )
